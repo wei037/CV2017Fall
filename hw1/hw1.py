2743a4800eef2bef17471db5a6d6387ba85218c6
r06922075 <r06922075@ntu.edu.tw>
2017-09-19 23:53:00 +0800

    Done OK.ver

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -3,14 +3,23 @@ from PIL import Image
 
 if len(sys.argv) != 5 :
 	print (len(sys.argv))
-	print ("pseudo lena.bmp lena_r.bmp lena_g.bmp lena_b.bmp\n")
+	print ("lena.bmp lena_r.bmp lena_g.bmp lena_b.bmp\n")
 	sys.exit()
 
-#pic = open(sys.argv[1],"rb").read()
 im = Image.open(sys.argv[1])
 pixels = im.load()
 
-for i in range(im.size[0]) :
-	for j in range(im.size[1]) :
-		pixels[i,j] =  (tuple(255-t for t in pixels[i,j]))
-im.save(sys.argv[2])
+im_r = Image.new('L', (im.size[0],im.size[1]), 'white')
+im_g = Image.new('L', (im.size[0],im.size[1]), 'white')
+im_b = Image.new('L', (im.size[0],im.size[1]), 'white')
+
+for i in range(int(im.size[0])) :
+	for j in range(int(im.size[1])) :
+		im_r.putpixel((i,j), 255 - pixels[i,j])
+		im_g.putpixel((i,j), 128 )
+		im_b.putpixel((i,j), pixels[i,j])
+
+
+im_r.save(sys.argv[2])
+im_g.save(sys.argv[3])
+im_b.save(sys.argv[4])
