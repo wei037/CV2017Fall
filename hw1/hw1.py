790fd19bf08deea1c49978b7d7e7bd898b5a45ea
r06922075_Lab <r06922075@ntu.edu.tw>
2017-09-20 19:31:26 +0800

    Correct one

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -1,25 +1,18 @@
-import sys
 from PIL import Image
 
-if len(sys.argv) != 5 :
-	print (len(sys.argv))
-	print ("lena.bmp lena_r.bmp lena_g.bmp lena_b.bmp\n")
-	sys.exit()
-
-im = Image.open(sys.argv[1])
+im = Image.open('lena.bmp')
 pixels = im.load()
 
-im_r = Image.new('L', (im.size[0],im.size[1]), 'white')
-im_g = Image.new('L', (im.size[0],im.size[1]), 'white')
-im_b = Image.new('L', (im.size[0],im.size[1]), 'white')
+im_u_d = Image.new('L', (im.size[0],im.size[1]), 'white')#upside-down
+im_r_l = Image.new('L', (im.size[0],im.size[1]), 'white')#sight-side-left
+im_d_m = Image.new('L', (im.size[0],im.size[1]), 'white')#diagonally mirrored
 
 for i in range(int(im.size[0])) :
 	for j in range(int(im.size[1])) :
-		im_r.putpixel((i,j), 255 - pixels[i,j])
-		im_g.putpixel((i,j), 128 )
-		im_b.putpixel((i,j), pixels[i,j])
+		im_u_d.putpixel((i,j), pixels[i , im.size[1]-j-1] )
+		im_r_l.putpixel((i,j), pixels[im.size[0]-i-1 , j] )
+		im_d_m.putpixel((i,j), pixels[j,i])
 
-
-im_r.save(sys.argv[2])
-im_g.save(sys.argv[3])
-im_b.save(sys.argv[4])
+im_u_d.save('lena_u_d.bmp')
+im_r_l.save('lena_r_l.bmp')
+im_d_m.save('lena_d_m.bmp')
